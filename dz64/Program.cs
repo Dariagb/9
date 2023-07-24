ca2c6d9f8ca811ecb49f20e20f6c58b72b595127
Darya Belousova <136568577+Dariagb@users.noreply.github.com>
2023-07-24 23:13:07 +0300

    Add files via upload

---
+++ dz64/Program.cs
@@ -0,0 +1,29 @@
+﻿ /*
+ Задача 64: Задайте значения N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1.
+ N = 5. -> "5, 4, 3, 2, 1"
+ N = 8. -> "8, 7, 6, 5, 4, 3, 2, 1"
+ */
+ 
+
+using System;
+using static System.Console;
+Clear();
+
+Write("Введите число N: ");
+int number = Convert.ToInt32(ReadLine());
+int count = 1;
+ResultNamber(number, count);
+
+
+void ResultNamber(int num, int count)
+{
+    if (count > num)
+    {
+        return;
+    }
+    else
+    {
+        ResultNamber(num, count + 1);
+        Write(count + " ");
+    }
+}
